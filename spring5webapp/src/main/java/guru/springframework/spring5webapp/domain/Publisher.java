6baff13b68fb808c9f14a2db36f2b039ea58ce54
ysnyldrm0 <ysnyldrm0@hotmail.com>
2021-07-09 21:47:45 +0300

    publisher configured

--- spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -25,6 +25,29 @@ public class Publisher {
     public Publisher() {
     }
 
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
 
     public String getName() {
         return name;
